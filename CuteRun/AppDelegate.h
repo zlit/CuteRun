a5673c25dc9bd976560538fae6a76f0451c1be0b
zlit <lizhaolei@LiZhaoleideMacBook-Air.local>
2014-10-02 18:53:10 +0800

    Initial Commit

---
+++ CuteRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CuteRun
+//
+//  Created by LiZhaolei on 14-10-2.
+//  Copyright (c) 2014年 com.zhaoleili.cuterun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
